2b43400afe1c766d4a1f0b99002557bb7cca61e4
moyasser20 <mohamedyasser192023@gmail.com>
2023-08-11 07:54:40 -0700

    g

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: prints the alphabet in lowercase followed by new line
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	int n = 0;
+
+	while (n <= 9)
+	{
+		putchar(n + '0');
+		n++
+	}
+	putchar("\n");
+
+	return (0);
+}
