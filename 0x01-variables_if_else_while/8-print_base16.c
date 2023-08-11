5c53c2afe12419b5e21db3ceeb04089534daac65
moyasser20 <mohamedyasser192023@gmail.com>
2023-08-11 08:13:07 -0700

    g

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: prints the alphabet in lowercase followed by new line
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	int n = 48;
+
+	while (n <= 102)
+	{
+		putchar(n);
+
+		if (n == 57)
+			n += 39;
+		n++;
+	}
+	putchar("\n");
+
+	return (0);
+}
