5c7886ce613cd6a44a9a71215ce1920a19e0745f
moyasser20 <mohamedyasser192023@gmail.com>
2023-08-11 08:03:58 -0700

    g

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *i
+ * Description: prints the alphabet in lowercase reversed followed by new line
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	char ch = 'z';
+
+	while (ch >= 'a')
+	{
+		putchar(ch);
+		ch--;
+	}
+	putchar("\n");
+
+	return (0);
+}
