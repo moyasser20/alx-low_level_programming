cfb6e03a4cd0b4c9a6d6d6c5b6f44e431c1de2dd
moyasser20 <mohamedyasser192023@gmail.com>
2023-08-15 11:41:32 -0700

    g

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -27,7 +27,7 @@ _putchar(' ');
 if (prod <= 9)
 _putchar(' ');
 else
-_putchar((prod / 10) + 48); /*get the first digit*/
+{_putchar((prod / 10) + 48);} /*get the first digit*/
 _putchar((prod % 10) + 48); /*get the second digit*/
 }
 _putchar('\n');
