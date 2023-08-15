4bafb53571e9e7988ca5b9a33862f144b5b1f7ee
moyasser20 <mohamedyasser192023@gmail.com>
2023-08-15 10:07:43 -0700

    g

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,20 +12,19 @@
 
 int print_sign(int n)
 {
-
-if (n > 0)
-{
-putchar(43);
-return (1);
-}
-else if (n == 0)
-{
-putchar(48);
-return (0);
-}
-else
-{
-putchar(45);
-return (-1);
-}
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else
+	{
+		_putchar(45);
+		return (-1);
+	}
 }
