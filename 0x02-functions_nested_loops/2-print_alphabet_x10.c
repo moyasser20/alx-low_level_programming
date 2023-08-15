b8a5554f876a89efd276e225aa805a284dd147fa
moyasser20 <mohamedyasser192023@gmail.com>
2023-08-15 08:29:09 -0700

    g

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 -  Pirnt th alphabet in lower case by function 10 times
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+int ch;
+
+for (int i = 0 ; i <= 9 ; i++)
+{
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+_putchar(ch);
+}
+_putchar('\n');
+}
+}
