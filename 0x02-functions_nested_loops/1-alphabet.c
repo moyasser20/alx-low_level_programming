40a922f9157ea917511319b4ad3045fa01fcf965
moyasser20 <mohamedyasser192023@gmail.com>
2023-08-15 08:05:57 -0700

    g

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,12 +7,9 @@ include "main.h"
 
 void print_alphabet(void)
 {
-char ch = 'a';
+	int ch;
 
-while (ch <= 'z')
-{
-putchar(ch);
-ch++;
-}
-putchar('\n');
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+		_putchar(ch);
+	_putchar('\n');
 }
