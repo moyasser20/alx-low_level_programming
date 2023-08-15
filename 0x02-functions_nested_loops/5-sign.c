43a9eeb73157f06a7f2b163cfd28c88f538cac32
moyasser20 <mohamedyasser192023@gmail.com>
2023-08-15 10:14:07 -0700

    g

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -14,17 +14,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		putchar(43);
+	        printf("+");
 		return (1);
 	}
 	else if (n == 0)
 	{
-		putchar(48);
+		printf("0");
 		return (0);
 	}
 	else
 	{
-		putchar(45);
+		printf("-");
 		return (-1);
 	}
 }
