7a0172eaec42c2a56da594cd0a4e6a3b2cb18253
moyasser20 <mohamedyasser192023@gmail.com>
2023-08-15 08:32:36 -0700

    g

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,14 +6,12 @@
 
 void print_alphabet_x10(void)
 {
-int ch;
+	int i, ch;
 
-for (int i = 0 ; i <= 9 ; i++)
-{
-for (ch = 'a' ; ch <= 'z' ; ch++)
-{
-_putchar(ch);
-}
-_putchar('\n');
-}
+	for (i = 0; i <= 9; i++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+			_putchar(ch);
+		_putchar('\n');
+	}
 }
