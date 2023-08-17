191514f1a135e41e81a037bc01c20a2dec4aa681
moyasser20 <mohamedyasser192023@gmail.com>
2023-08-17 05:43:53 -0700

    g

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -5,11 +5,11 @@
  *
  * @c: input
  *
- * Return: 1 if digit,0 otherwise
+ * Return: 1 if digit,0 if not
  *
 */
 
-int _isdigit(int c);
+int _isdigit(int c)
 {
 	if (c >= 48 && c <= 57)
 		return (1);
