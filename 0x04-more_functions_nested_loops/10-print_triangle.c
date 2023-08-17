26fddf359e515c5777d1a3164a5a65d4e20a06c7
moyasser20 <mohamedyasser192023@gmail.com>
2023-08-17 08:52:43 -0700

    g

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main"
+/**
+* print_triangle - print a triangle followed by a new line
+* @size: size of the triangle
+*/
+
+void print_triangle(int size)
+{
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+	i	int i, j;
+
+		for (i = 1; i < size; i++)
+		{
+			for (j = i; j < size; j++)
+			{
+				_putchar(' ');
+			}
+			for (j = 1; j <= i; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
