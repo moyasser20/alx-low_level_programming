882120fc7f9440619120c24db3192a282cbada8c
moyasser20 <mohamedyasser192023@gmail.com>
2023-08-17 06:59:55 -0700

    g

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,19 +9,20 @@
 
 void more_numbers(void)
 {
-int i, num, count;
+	int row, num, count;
 
-for (i = 0; i <= 10; i++)
-{
-for (int count = 0; count <= 14; count++)
-{
-num = count;
-if (count > 9)
-_putchar(1 + 48);
-num = count % 10;
-}
-_putchar(num + 48);
-}
-_putchar('\n');
-}
+	for (row = 1; row <= 10 ; row++)
+	{
+		for (count = 0; count <= 14; count++)
+		{
+			num = count;
+			if (count > 9)
+			{
+				_putchar(1 + 48);
+				num = count % 10;
+			}
+			_putchar(num + 48);
+		}
+		_putchar('\n');
+	}
 }
