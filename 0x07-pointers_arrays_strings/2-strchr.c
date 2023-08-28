47bbf58b57398fdbbb372db6409d0bc298130923
moyasser20 <mohamedyasser192023@gmail.com>
2023-08-28 04:35:24 -0700

    g

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,24 +1,25 @@
 #include "main.h"
 
 /**
-* _strchr - fills memory with a constant type
-*
-* @s: pointer to put the constant
-* @c: constant
-*
-* Return: Pointerto s or null
+ * _strchr - bbe
+ * @s: ff
+ * @c: gg
+ * Return: Always 0
 */
 
 char *_strchr(char *s, char c)
 {
-	int n;
-
-	for (n = 0; s[n] >= '\0' ; n++)
-	{
-		if (s[n] == c)
-		{
-			return (s + itr);
-		}
-	}
-	return ('\0');
+while (*s != '\0')
+{
+if (*s == c)
+{
+return (s);
+}
+s++;
+}
+if (c == '\0')
+{
+return (s);
+}
+return (NULL);
 }
