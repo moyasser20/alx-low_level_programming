ce4bb6a7d10a840e0487dcd74a8adb1b032ddbd0
moyasser20 <mohamedyasser192023@gmail.com>
2023-08-28 04:16:01 -0700

    g

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* print_chessboard - print chessboard
+*
+* @a: Rows
+*
+* Return: Nothing
+*/
+
+void print_chessboard(char (*a)[8])
+{
+int i,n;
+
+for (i = 0; i < 8; i++)
+{
+for (n = 0; n < 8; n++)
+{
+_putchar(a[i][n]);
+}
+_putchar('\n');
+}
+}
