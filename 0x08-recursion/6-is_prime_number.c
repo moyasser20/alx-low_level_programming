40180231211a4dd30785b951a727773114bc82b3
moyasser20 <mohamedyasser192023@gmail.com>
2023-08-30 02:21:57 -0700

    g

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,36 +1,28 @@
 #include "main.h"
 
 /**
- * is_prime_number - Checks if a number is prime.
- * @n: The number to check.
- *
- * Return: 1 if @n is prime, 0 otherwise.
- */
+ * is_prime_number - bbe
+ * @n: ff
+ * @othern: number
+ * Return: Always 0
+*/
+int checkprime(int n, int othern);
 int is_prime_number(int n)
 {
-if (n <= 1)
-{
-return (0);  /* 0 and 1 are not prime */
+return (checkprime(n, 2));
 }
-return (is_prime_helper(n, 2)); /* Call the helper function */
-}
-
 /**
- * is_prime_helper - Recursive helper function to check if a number is prime.
- * @n: The number to check.
- * @divisor: The current divisor being checked.
- *
- * Return: 1 if @n is prime, 0 otherwise.
- */
-int is_prime_helper(int n, int divisor)
+ * checkprime - bbe
+ * @n: rf
+ * @othern: number
+ * Return: Always 0
+*/
+int checkprime(int n, int othern)
 {
-if (divisor == n)
-{
-return (1);  /* Base case: Reached the end without finding a divisor */
-}
-if (n % divisor == 0)
-{
-return (0);  /* @n is divisible by @divisor, not prime */
-}
-return (is_prime_helper(n, divisor + 1));
+if (othern >= n && n > 1)
+return (1);
+else if (n % othern == 0 || n <= 1)
+return (0);
+else
+return (checkprime(n, othern + 1));
 }
