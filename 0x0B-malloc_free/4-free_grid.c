16e0a387b3527aae2bdc1e7a9b3dd6163c11f63e
moyasser20 <mohamedyasser192023@gmail.com>
2023-09-04 10:39:02 -0700

    g

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,20 @@
+#include <main.h>
+
+/**
+* free_grid - bb
+* @grid: pointer
+* @height:bb
+* Return: void
+*/
+
+void free_grid(int **grid, int height)
+{
+int i = 0;
+
+for (; i < height; i++)
+{
+free(grid[i]);
+}
+free(grid);
+}
+
