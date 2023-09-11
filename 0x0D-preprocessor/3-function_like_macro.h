d207300e01ebf64301fb665ec4838190ac837339
moyasser20 <mohamedyasser192023@gmail.com>
2023-09-11 02:35:29 -0700

    g

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
-#ifndef _FUNCTION_LIKE_MICRO_H
-#define _FUNCTION_LIKE_MICRO_H
+#ifndef _FUNCTION_LIKE_MACRO_H
+#define _FUNCTION_LIKE_MACRO_H
 
 #define ABC(x) ((x) < 0 ? (-(x)) : (x))
 
-#endif /* _FUNCTION_LIKE_MICRO_H */
+#endif /* _FUNCTION_LIKE_MACRO_H */
