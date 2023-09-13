130758ae5ceb3d6b8edb8487827a952c148eaedd
moyasser20 <mohamedyasser192023@gmail.com>
2023-09-13 04:41:10 -0700

    g

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,21 +1,15 @@
 #include "function_pointers.h"
-
 /**
-* array_iterator - ff
-* @array: sdf
-* @size: dd
-* @action: hh
-* Return: void
+ * array_iterator - bbe
+ * @array: ff
+ * @size: gg
+ * @action: gg
+ * Return: Always 0
 */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 int *end = array + size - 1;
-
 if (array && size && action)
-{
-while (array < end)
-{
+while (array <= end)
 action(*array++);
-}
-}
 }
