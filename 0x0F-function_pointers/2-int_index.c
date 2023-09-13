5c7d81fc92cc6f37f55ca0176f5c7fd9b2530b0f
moyasser20 <mohamedyasser192023@gmail.com>
2023-09-13 04:53:28 -0700

    g

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,12 +1,12 @@
 #include "function_pointers.h"
 
 /**
-* int_index - ff
-* @array: sdf
-* @size: dd
-* @cmp: hh
-* Return: i or -1
-*/
+ * int_index - ff
+ * @array: sdf
+ * @size: dd
+ * @cmp: hh
+ * Return: i or -1
+ */
 int int_index(int *array, int size, int (*cmp)(int))
 {
 int i = 0;
@@ -19,9 +19,8 @@ if (cmp(array[i]))
 {
 return (i);
 }
-i++
+i++;
 }
 }
 return (-1);
 }
-
