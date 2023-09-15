d773426181c0ce6ee709f7d4992ccfba005e4795
moyasser20 <mohamedyasser192023@gmail.com>
2023-09-15 14:35:37 -0700

    g

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,37 +1,23 @@
-#include <stdarg.h>
-#include <stdio.h>
 #include "variadic_functions.h"
 /**
- * print_numbers - ss
- * @separator: jj
- * @n: kk
- * @...: aa
- * Return: Always 0.
- */
+ * print_numbers - bbe
+ * @separator: ff
+ * @n: gg
+ * @...: gg
+ * Return: Always 0
+*/
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-va_list num;
-int i;
-
+int i = n;
+va_list ap;
 if (!n)
 {
 printf("\n");
 return;
 }
-
-va_start(num, n);
-
-for (i = 0; i < n; i++)
-{
-printf("%d", va_arg(num, int));
-if (i < n - 1 && separator != NULL)
-printf("%s", separator);
+va_start(ap, n);
+while (i--)
+printf("%d%s", va_arg(ap, int), i ? (separator ? separator : "") : "\n");
+va_end(ap);
 }
 
-if (separator == NULL)
-{
-printf("\n");
-}
-
-va_end(num);
-}
